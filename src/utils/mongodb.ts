21f94db431bc2e9c3788bad758cfbe3bf6be7fa5
Your Name <marc@lemedioni.fr>
2024-07-01 19:26:18 +0200

    linting

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -10,13 +10,12 @@
    if (!process.env.MONGODB_URI) {
      throw new Error('Please add your Mongo URI to .env.local');
    }
-
    if (process.env.NODE_ENV === 'development') {
-     if (!global._mongoClientPromise) {
+     if (!(global as any)._mongoClientPromise) {
        client = new MongoClient(uri, options);
-       global._mongoClientPromise = client.connect();
+       (global as any)._mongoClientPromise = client.connect();
      }
-     clientPromise = global._mongoClientPromise;
+     clientPromise = (global as any)._mongoClientPromise;
    } else {
      client = new MongoClient(uri, options);
      clientPromise = client.connect();
